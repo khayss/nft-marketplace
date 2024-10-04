0b60497e0a61d100df9f6dd491e982261ceca4b3
khayss <forendals@gmail.com>
2024-10-04 10:30:22 +0100

    feat: add lisk sepolia config and deploy on lisk sepolia

--- ignition/modules/NftMarketplace.ts
+++ ignition/modules/NftMarketplace.ts
@@ -1,6 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const OWNER_ADDRESS = "";
+const OWNER_ADDRESS = "0xDaB8892C07FB4C362Dd99D9a2fBFf8B555D39Cb5";
 
 const NftMarketplaceModule = buildModule("NftMarketplaceModule", (m) => {
   const ownerArgs = m.getParameter("owner", OWNER_ADDRESS);
